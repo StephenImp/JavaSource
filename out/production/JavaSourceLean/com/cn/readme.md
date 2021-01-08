120bb1805ae9647232b466bc9699368a1500ffe5
e <lmp@douyu.tv>
2021-01-08 17:33:10 +0800

    update

---
+++ out/production/JavaSourceLean/com/cn/readme.md
@@ -0,0 +1,3 @@
+### JDK源码阅读环境搭建
+
+##### https://www.cnblogs.com/jiazhongxin/p/12774587.html
---
+++ src/com/cn/test/hashMap/TestHashMapCase.java
@@ -0,0 +1,16 @@
+package com.cn.test.hashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestHashMapCase {
+
+    public static void main(String[] args) {
+
+        Map<String,Double> map = new HashMap<>();
+
+        map.put("K1",1D);
+        map.put("K2",2D);
+
+    }
+}
